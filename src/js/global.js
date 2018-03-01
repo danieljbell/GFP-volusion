f125ceb9472f337f0537519dffb7bb64b7aa0dd9
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-03-01 17:01:51 -0500

    wayyyyy too much

--- src/js/global.js
+++ src/js/global.js
@@ -1,8 +1,8 @@
 (function() {
 
     document.addEventListener('click', function(e) {
-        
-        if (e.target.classList.contains('menu-toggle')) {
+
+        if ((e.target.id === 'hamburger') || (e.target.classList.contains('hamburger-box')) || (e.target.classList.contains('hamburger-inner'))) {
             toggleMenu();
         }
 
@@ -16,12 +16,24 @@
 
     });
 
+    // document.addEventListener('mouseover', function(e) {
+
+    //     if (e.target.classList.contains('accordian-toggle--site-header')) {
+    //         if (window.innerWidth > 1080) {
+    //             toggleMenuAccordian(e);
+    //         }
+    //     }
+    // });
+
     function toggleMenu() {
         document.body.classList.toggle('menu-open');
+        var menuButton = document.querySelector('#hamburger');
+        menuButton.classList.toggle('is-active');
     }
 
     function toggleMenuAccordian(e) {
         var self = e.target;
+        console.log(self);
         if (!self.nextElementSibling) { return }
         self.classList.toggle('active');
         self.nextElementSibling.classList.toggle('accordian-open');
