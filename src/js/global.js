e4fd05398215fc5beb8d1294e93f3d379bbff30b
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-03-02 17:03:12 -0500

    dynamic images, sticky search

--- src/js/global.js
+++ src/js/global.js
@@ -1,51 +1,97 @@
-(function() {
+var pagePath = window.location.pathname;
 
-    document.addEventListener('click', function(e) {
+var heroImg = 'url(/v/vspfiles/templates/gfp-test/img/hero--generic-' + getRandomInt(1, 5) + '.jpg)';
+var heroText = 'Green Farm Parts';
 
-        if ((e.target.id === 'hamburger') || (e.target.classList.contains('hamburger-box')) || (e.target.classList.contains('hamburger-inner'))) {
-            toggleMenu();
-        }
+if (pagePath.match(/Filters/i)) {
+    heroImg = 'url(/v/vspfiles/templates/gfp-test/img/hero--filters.jpg)';
+    heroText = 'Oil Filters';
+}
 
-        if (e.target.classList.contains('accordian-toggle--site-header')) {
-            toggleMenuAccordian(e);
-        }
+if (pagePath.match(/Maintenance-Kits/i)) {
+    heroImg = 'url(/v/vspfiles/templates/gfp-test/img/hero--maintenance-kits.jpg)';
+    heroText = 'Maintenance Kits';
+}
 
-        if (e.target.hash === '#chat') {
-            toggleChatWindow(e);
-        }
+if (pagePath.match(/Toys/i)) {
+    heroImg = 'url(/v/vspfiles/templates/gfp-test/img/hero--toys.jpg)';
+    heroText = 'Toys';
+}
 
-    });
+updateHero(heroImg, heroText);
 
-    // document.addEventListener('mouseover', function(e) {
+var breadcrumbs = document.querySelector('.matching_results_text');
 
-    //     if (e.target.classList.contains('accordian-toggle--site-header')) {
-    //         if (window.innerWidth > 1080) {
-    //             toggleMenuAccordian(e);
-    //         }
-    //     }
-    // });
+if (breadcrumbs) {
+    breadcrumbs.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList.add('breadcrumbs');
+}
 
-    function toggleMenu() {
-        document.body.classList.toggle('menu-open');
-        var menuButton = document.querySelector('#hamburger');
-        menuButton.classList.toggle('is-active');
+var searchBarContainer = document.querySelector('.search-bar-container');
+var searchBarContainerOffset = searchBarContainer.offsetTop;
+
+if (searchBarContainer) {
+    window.addEventListener('scroll', fixSearch);
+}
+
+document.addEventListener('click', function(e) {
+
+    if ((e.target.id === 'hamburger') || (e.target.classList.contains('hamburger-box')) || (e.target.classList.contains('hamburger-inner'))) {
+        toggleMenu();
     }
 
-    function toggleMenuAccordian(e) {
-        var self = e.target;
-        console.log(self);
-        if (!self.nextElementSibling) { return }
-        self.classList.toggle('active');
-        self.nextElementSibling.classList.toggle('accordian-open');
+    if (e.target.classList.contains('accordian-toggle--site-header')) {
+        toggleMenuAccordian(e);
     }
 
-    function toggleChatWindow(e) {
-        e.preventDefault();
-        if (window.fcWidget.isOpen() != true) {
-            window.fcWidget.open();
-        } else {
-            window.fcWidget.close();
-        }
+    if (e.target.hash === '#chat') {
+        toggleChatWindow(e);
     }
 
-})();
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+    var menuButton = document.querySelector('#hamburger');
+    menuButton.classList.toggle('is-active');
+}
+
+function toggleMenuAccordian(e) {
+    var self = e.target;
+    console.log(self);
+    if (!self.nextElementSibling) { return }
+    self.classList.toggle('active');
+    self.nextElementSibling.classList.toggle('accordian-open');
+}
+
+function toggleChatWindow(e) {
+    e.preventDefault();
+    if (window.fcWidget.isOpen() != true) {
+        window.fcWidget.open();
+    } else {
+        window.fcWidget.close();
+    }
+}
+
+function updateHero(heroImg, heroText) {
+    var heroContainer = document.querySelector('.hero--not-home');
+    heroContainer.style.backgroundImage = heroImg;
+    var heroHeading = heroContainer.querySelector('h1');
+    heroHeading.innerHTML = heroText;
+    heroHeading.style.opacity = 1;
+}
+
+function fixSearch() {
+    if (window.scrollY >= searchBarContainerOffset) {
+        document.body.style.paddingTop = searchBarContainer.offsetHeight + 'px';
+        document.body.classList.add('search-bar-fixed');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('search-bar-fixed');
+    }
+}
